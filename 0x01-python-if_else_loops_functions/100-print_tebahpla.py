ef977af778a4a742e807686d33f13ae927ddefd5
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-11-28 11:41:04 -0800

    prints the ASCII alphabet, in reverse order

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+i = 32
+for c in range(ord('z', ord('a') -1, -1):
+        if c % 2 != 0:
+            print("{:c}".format(c - i), end="")
+        else:
+            print("{:c}".format(c), end="")
+
