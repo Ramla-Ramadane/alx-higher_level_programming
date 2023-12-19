f9a39bab5b70b80ad264b167a69581901ef7dc72
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-12-19 15:16:05 -0800

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,7 @@ import math
 class MagicClass:
     def __init__(self, radius=0):
         self.__radius = 0
-        if type(radius) != int and type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             print("radius must be a number", end='')
             raise TypeError
         else:
