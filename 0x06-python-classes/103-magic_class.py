aa779248c6219644a8fb3abd7532dbd9c37cf5e2
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-12-19 15:09:46 -0800

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import math
+
 class MagicClass:
-    import math
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) != int and type(radius) is not float:
