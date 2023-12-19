e7fbeb1b49cce4b5151f439f9f9131318c240c12
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-12-19 11:56:22 -0800

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''class Square that defines a square by:
+(based on 1-square.py)'''
+
+
+class Square:
+    '''The class square'''
+    def __int__(self, size=0):
+        if type(size) not int:
+            raise TypeError("size must be an integer", end='')
+        elif size < 0:
+            print("size must be >= 0", end='')
+            raise ValueError
+        self._size = size
