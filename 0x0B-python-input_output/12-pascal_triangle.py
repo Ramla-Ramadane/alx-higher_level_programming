22307df079d49094ff27b5f0821bcaa16853c5db
Ramla-Ramadane <ramlaramadane@gmail.com>
2024-02-01 02:56:59 -0800

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,15 +3,22 @@
 
 
 def pascal_triangle(n):
+    ls_1 = []
     if n <= 0:
-        return []
-
-    triangle = [[1]]
-    while len(triangle) != n:
-        tri = triangle[-1]
-        tmp = [1]
-        for idx in range(len(tri) - 1):
-            tmp.append(tri[idx] + tri[idx + 1])
-        tmp.append(1)
-        triangle.append(tmp)
-    return triangle
+        return ls_1
+    for nu in range(0, n):
+        if nu == 0:
+            ls_1.append([1])
+        else:
+            ls_2 = []
+            len_last = len(ls_1[-1])
+            idx = len(ls_1) - 1
+            for nu_2 in range(0, len_last):
+                if nu_2 == 0:
+                    ls_2.append(1)
+                if nu_2 == len_last - 1:
+                    ls_2.append(1)
+                else:
+                    ls_2.append(ls_1[idx][nu_2] + ls_1[idx][nu_2 + 1])
+            ls_1.append(ls_2)
+    return ls_1
