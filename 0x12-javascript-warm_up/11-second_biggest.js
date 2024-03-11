5bf7366410842efd66dca07ae71fc445435cbd29
Ramla-Ramadane <ramlaramadane@gmail.com>
2024-03-11 04:57:26 -0700

    javascript warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,8 @@
 #!/usr/bin/node
-
-const getArgument = function (myArray) {
-  for (let i = 0; i < myArray.length; i++) {
-    if (isNaN(myArray[i])) {
-      myArray.splice(i, i);
-    }
-  }
-  for (let i = 0; i < myArray.length; i++) {
-    myArray[i] = parseInt(myArray[i]);
-  }
-
-  return myArray;
-};
-
-const process = require('process');
-
-const myArray = process.argv.slice(2, process.argv.length);
-if (myArray < 2) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  console.log(getArgument(myArray)
-    .sort().reverse()[1]);
+  const myarray = process.argv.slice(2).map(Number);
+  const second = myarray.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
