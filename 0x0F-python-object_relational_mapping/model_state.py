6a60dc5167c536cccb41bd77a58f98f3a624494c
Ramla-Ramadane <ramlaramadane@gmail.com>
2024-03-19 04:28:55 -0700

    python ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,11 +2,16 @@
 '''python file that contains the class definition of a state'''
 
 
-from model_state import Base, State
-import sys
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+class State(Base):
+    '''class state'''
+
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
